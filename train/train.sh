eec3de040d44f305e462b71c7fd2b6d91d48795d
xiajun <xiajun@idea.edu.cn>
2022-03-03 15:56:30 +0800

    Ignore train log

--- train/train.sh
+++ train/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1 python train.py \
+CUDA_VISIBLE_DEVICES=1 python -u train.py \
         --model_name_or_path "$HOME/models/pubmedbert_abs" \
         --train_text_file '/platform_tech/aigraph/data/entity_type_classify/sty_classifer_train_text_0303.txt' \
 	--train_match_file '/platform_tech/aigraph/bios_ner/preprocess/task_sty_cls/sty_classifer_train_text_0303_tagged.txt' \
@@ -8,4 +8,5 @@ CUDA_VISIBLE_DEVICES=1 python train.py \
 	--output_dir 'output-c5.4ex-0303' \
 	--do_train \
 	--device 'cuda:1' \
-        --train_batch_size 512 
+        --train_batch_size 512 \
+        > train_0303.log 2>&1 &
