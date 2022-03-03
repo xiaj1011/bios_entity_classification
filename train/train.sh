f8a69c5a732ca962eaed50fff23f9b80b9696c19
xiajun <xiajun@idea.edu.cn>
2022-03-03 15:49:51 +0800

    Move dict directory

--- train/train.sh
+++ train/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
+CUDA_VISIBLE_DEVICES=1 python train.py \
         --model_name_or_path "$HOME/models/pubmedbert_abs" \
-        --train_text_file '../example/data/corpus.txt' \
-	--train_match_file '../example/data/corpus_tagged.txt' \
-	--clean_term_path '../example/cleanterms/cleanterms.txt.example' \
-	--output_dir 'output' \
+        --train_text_file '/platform_tech/aigraph/data/entity_type_classify/sty_classifer_train_text_0303.txt' \
+	--train_match_file '/platform_tech/aigraph/bios_ner/preprocess/task_sty_cls/sty_classifer_train_text_0303_tagged.txt' \
+	--clean_term_path '/platform_tech/aigraph/cleanterms/c5_4ex/cleanterms5_4ex.txt' \
+	--output_dir 'output-c5.4ex-0303' \
 	--do_train \
-	--device 'cuda:0' \
+	--device 'cuda:1' \
         --train_batch_size 512 
--- utils/label_util.py
+++ utils/label_util.py
@@ -3,7 +3,7 @@ import json
 def get_entity_type_from_train_file():
     sty_set = set()
     sty2sgr = {}
-    with open("../example/cleanterms/cleanterms.txt", "r", encoding="utf-8") as f:
+    with open("/platform_tech/aigraph/cleanterms/cleanterms5_4ex.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()[1:]
     for line in lines:
         stys = line.split("\t")[2]
@@ -14,16 +14,16 @@ def get_entity_type_from_train_file():
     sty_list = list(sty_set)
     sty_list.sort()
     sty2id = {sty:idx for idx, sty in enumerate(sty_list)}
-    with open("./dict/entity_type.json", "w", encoding="utf-8") as f:
+    with open("../dict/entity_type.json", "w", encoding="utf-8") as f:
         json.dump(sty2id, f)
-    with open("./dict/entity_group.json", "w", encoding="utf-8") as f:
+    with open("../dict/entity_group.json", "w", encoding="utf-8") as f:
         json.dump(sty2sgr, f)
 
 
 def get_entity_type_from_json():
-    with open("../example/cleanterms/entity_type.json", "r", encoding="utf-8") as f:
+    with open("../dict/entity_type.json", "r", encoding="utf-8") as f:
         sty2id = json.loads(f.readline())
-    with open("../example/cleanterms/entity_group.json", "r", encoding="utf-8") as f:
+    with open("../dict/entity_group.json", "r", encoding="utf-8") as f:
         sty2sgr = json.loads(f.readline())
     return sty2id, sty2sgr
 
@@ -31,7 +31,7 @@ def get_entity_type_from_json():
 def check_entity_type():
     sty_set = set()
     sty2sgr = {}
-    with open("../example/cleanterms/cleanterms.txt", "r", encoding="utf-8") as f:
+    with open("/platform_tech/aigraph/cleanterms/cleanterms5_4ex.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()[1:]
     for line in lines:
         stys = line.split("\t")[2]
@@ -44,5 +44,6 @@ def check_entity_type():
     print(sty2sgr)
 
 if __name__ == "__main__":
+    get_entity_type_from_train_file()
     check_entity_type()
 
