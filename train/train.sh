5a5a1bbf41c0560aada768b2118553aa2c9bf2e6
xiajun <xiajun@idea.edu.cn>
2022-03-23 15:16:49 +0800

    Update train.sh

--- train/train.sh
+++ train/train.sh
@@ -2,17 +2,17 @@
 
 CUDA_VISIBLE_DEVICES='1' python -u train.py \
         --model_name_or_path "$HOME/models/pubmedbert_abs" \
-        --train_text_file '../data/v5_term_indexing/term_train_v5.txt' \
-	--train_match_file '../data/v5_term_indexing/term_train_tagged_v5.txt' \
-        --eval_text_file '../data/v5_term_indexing/term_eval_v5.txt' \
-        --eval_match_file '../data/v5_term_indexing/term_eval_tagged_v5.txt' \
-	--clean_term_path '../data/merge_cleanterms.txt' \
-	--output_dir 'output_merge_cleanterms_v5_ep4' \
+        --train_text_file '../scripts/other_sty/term_train_v5.1.txt' \
+	--train_match_file '../scripts/other_sty/term_train_tagged_v5.1.txt' \
+        --eval_text_file '../scripts/other_sty/term_test.txt' \
+        --eval_match_file '../scripts/other_sty/term_test_tagged.txt' \
+	--clean_term_path '../data/merge_cleanterms_25w_other.txt' \
+	--output_dir 'output_merge_cleanterms_v5.1_ep3' \
 	--do_train \
 	--device 'cuda:0' \
         --train_batch_size 512 \
         --learning_rate 1e-4 \
-        --train_epoch 4 \
+        --train_epoch 3 \
         --save_step 500 \
         --eval_step 500 \
-        > train_v5.log 2>&1 &
+        > train_v5.1.log 2>&1 &
